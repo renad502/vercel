1d079a41c029f2c5fdba9c980d3bc0647f5affff
Renad <nainarenad@gmail.com>
2025-05-21 23:05:39 -0700

    update project

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
-async def health_check():
+def health_check():
     return {"message": "The health check is successful"}
 
-handler = Mangum(app)
