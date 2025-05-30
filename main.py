06cf6981a9fbf5b1fb5f407288830e8c9bd85415
Renad <nainarenad@gmail.com>
2025-05-30 04:05:17 -0700

    update main

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ app = FastAPI()
 @app.post("/register", response_model=UserOut)
 async def register(user: UserCreate):
     return await register_user(user)
+@app.get("/")
+def read_root():
+    return {"message":"hi deploy ho gya jy! Hurraahh"}
